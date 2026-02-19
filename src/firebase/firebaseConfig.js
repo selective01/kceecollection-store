77a6c0e296d29e0b4e31dc19556f22823de9490e
Kceecollection <53386215+selective01@users.noreply.github.com>
2026-02-19 11:02:55 +0100

    Ready for deployment

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 
 // Your Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyBEVm0IG2FFIBe9m7V1QuE21lSt7wyJfRI",
-  authDomain: "kceecollection-72032.firebaseapp.com",
-  projectId: "kceecollection-72032",
-  storageBucket: "kceecollection-72032.appspot.com",
-  messagingSenderId: "102701466324",
-  appId: "1:102701466324:web:938d56001da334e3bff141"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
